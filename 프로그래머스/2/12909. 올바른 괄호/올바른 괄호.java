85d3bfbfc2459012b2027b798cd482f6eb060eb9
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2025-08-14 22:12:48 +0900

    [level 2] Title: 올바른 괄호, Time: 19.38 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,3 +1,5 @@
+// 
+
 import java.util.*;
 
 class Solution {
@@ -5,17 +7,29 @@ class Solution {
         boolean answer = true;
 
         Stack<Character> stack = new Stack<>();
-        Character[] characterArray = s.chars()
-            .mapToObj(c->(char)c)
-            .toArray(Character[]::new);
+        // Character[] characterArray = s.chars()
+        //     .mapToObj(c->(char)c)
+        //     .toArray(Character[]::new);
         
-        for (int i = 0; i < characterArray.length; i++) {
-            if (characterArray[i].equals('(')) {
-                stack.push(characterArray[i]);
-            } else if(stack.size() == 0) {
-                return false;
-            } else {
+        char[] charArray = s.toCharArray();
+        
+        // for (int i = 0; i < characterArray.length; i++) {
+        //     if (characterArray[i].equals('(')) {
+        //         stack.push(characterArray[i]);
+        //     } else if(stack.size() == 0) {
+        //         return false;
+        //     } else {
+        //         stack.pop();
+        //     }
+        // }
+        
+        for (char c : charArray) {
+            if ((c == '(')) {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty()) {
                 stack.pop();
+            } else {
+                return false;
             }
         }
         

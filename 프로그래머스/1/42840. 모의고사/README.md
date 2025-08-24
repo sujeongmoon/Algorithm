8d364b7fff9d9d6ef482e39bf49c981a33b3aaed
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2025-08-25 01:51:17 +0900

    [level 1] Title: 모의고사, Time: 3.05 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
 
 ### 성능 요약
 
-메모리: 9.27 MB, 시간: 5.41 ms
+메모리: 84.5 MB, 시간: 3.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 01일 21:51:05
+2025년 08월 25일 01:51:15
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,34 +1,82 @@
 import java.util.*;
 
-// 코플릿 
 class Solution {
-    
     public int[] solution(int[] answers) {
-        int[] patterns = {0, 0, 0}; // 수포자의 점수
-        int[][] dartPatterns = {
-            {1, 2, 3, 4, 5},
-            {2, 1, 2, 3, 2, 4, 2, 5},
-            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-        };
-
-        // 각 수포자의 정답 점수를 계산
-        for (int i = 0; i < answers.length; i++) {
-            for (int j = 0; j < dartPatterns.length; j++) {
-                if (answers[i] == dartPatterns[j][i % dartPatterns[j].length]) {
-                    patterns[j]++;
+        // int[] answer = {};
+        int [] students = {0, 0, 0};
+        
+        for (int i = 0; i < answers.length; i ++) {
+            if (answers[i] == 1) { // 정답이 1인 경우
+                if ((i+1) % 5 == 1) {
+                    students[0]++;
+                }
+                if ((i+1) % 8 == 2) {
+                    students[1]++;
+                }
+                if ((i+1) % 10 == 3 || (i+1) % 10 == 4) {
+                    students[2]++;
+                }
+            } else if (answers[i] == 2) { //정답이 2인 경우
+                if ((i+1) % 5 == 2) {
+                    students[0]++;
+                }
+                if (((i+1) % 8) % 2 == 1) {
+                    students[1]++;
+                }
+                if ((i+1) % 10 == 5 || (i+1) % 10 == 6) {
+                    students[2]++;
+                }
+            } else if (answers[i] == 3) { //정답이 3인 경우
+                if ((i+1) % 5 == 3) {
+                    students[0]++;
+                }
+                if ((i+1) % 8 == 4) {
+                    students[1]++;
+                }
+                if ((i+1) % 10 == 1 || (i+1) % 10 == 2) {
+                    students[2]++;
+                }
+            } else if (answers[i] == 4) { //정답이 4인 경우
+                if ((i+1) % 5 == 4) {
+                    students[0]++;
+                }
+                if ((i+1) % 8 == 6) {
+                    students[1]++;
+                }
+                if ((i+1) % 10 == 7 || (i+1) % 10 == 8) {
+                    students[2]++;
+                }
+            } else if (answers[i] == 5) { //정답이 5인 경우
+                if ((i+1) % 5 == 0) {
+                    students[0]++;
+                }
+                if ((i+1) % 8 == 0) {
+                    students[1]++;
+                }
+                if ((i+1) % 10 == 9 || (i+1) % 10 == 0) {
+                    students[2]++;
                 }
             }
+            
         }
-
-        // 최고 점수를 받은 수포자 찾기
-        int maxScore = Arrays.stream(patterns).max().getAsInt();
-        List<Integer> maxList = new ArrayList<>();
-        for (int i = 0; i < patterns.length; i++) {
-            if (patterns[i] == maxScore) {
-                maxList.add(i + 1);
+        
+        // 최댓값을 찾는다
+        int max = 0;
+        for (int i = 0; i < students.length; i++) {
+            if (max < students[i]) {
+                max = students[i];
             }
         }
-
-        return maxList.stream().mapToInt(i -> i).toArray();
+        
+        // 최댓값과 동일한 수를 맞춘 학생을 찾는다
+        List<Integer> answerList = new ArrayList<>();
+        
+        for (int i = 0; i < students.length; i++) {
+            if (max == students[i]){
+                answerList.add(i+1);
+            }
+        }       
+        
+        return answerList.stream().mapToInt(Integer::intValue).toArray();
     }
 }

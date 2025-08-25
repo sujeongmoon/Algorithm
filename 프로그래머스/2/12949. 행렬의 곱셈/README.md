bf06a352637c9754fe48639115c615896e9ef5ce
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2025-08-25 23:47:40 +0900

    [level 2] Title: 행렬의 곱셈, Time: 10.59 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=java) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 99.38 ms
+메모리: 81.7 MB, 시간: 10.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 02일 19:54:34
+2025년 08월 25일 23:47:36
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         
-        int r1 = arr1.length;
-        int c1 = arr1[0].length;
-        int r2 = arr2.length;
-        int c2 = arr2[0].length;
+        int a = arr1.length;
+        int b = arr1[0].length;
+        int c = arr2[0].length;
         
-        int[][] answer = new int[r1][c2];
+        int[][] answer = new int[a][c];
         
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c2; j++) {
-                for (int k = 0; k < c1; k++) { // 두 행렬의 데이터를 곱해 결과 리스트에 더하기
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < c; j++) {
+                for (int k = 0; k < b; k++) {
                     answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
             }

02267f9c335fe7daa91531c99dd6fc844adf9767
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2025-10-23 23:22:48 +0900

    StringTokenizer 추가
    Added StringTokenizer to read input tokens.

--- etc/Main.java
+++ etc/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while (st.hasMoreTokens()) {
+            String token st.nextToken();
+        }
         
         //솔루션 코드 작성
         

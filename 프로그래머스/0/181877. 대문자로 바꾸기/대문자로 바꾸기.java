5698a4e4e7b9ffc7f6501f247a38c0ff6c377312
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2025-10-24 22:38:03 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < myString.length(); i++) {
+            char c = myString.charAt(i);
+            if (c > 'Z') {
+                c -= 32;
+            }
+            sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}

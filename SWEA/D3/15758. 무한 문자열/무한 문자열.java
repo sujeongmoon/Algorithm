a229af45449ed1f84c58248c5ec6ce3832853469
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2025-11-03 16:25:07 +0900

    [D3] Title: 무한 문자열, Time: 75 ms, Memory: 24,448 KB -BaekjoonHub

--- SWEA/D3/15758. 무한 문자열/무한 문자열.java
+++ SWEA/D3/15758. 무한 문자열/무한 문자열.java
@@ -20,42 +20,16 @@ class Solution
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
             StringTokenizer st = new StringTokenizer(br.readLine());
-            String shortWord = st.nextToken();
-            String longWord = st.nextToken();
+            String a = st.nextToken();
+            String b = st.nextToken();
             StringBuilder sb = new StringBuilder();
+            StringBuilder sb1 = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
             
-            if (shortWord.length() > longWord.length()) {
-                String temp = longWord;
-                longWord = shortWord;
-                shortWord = temp;
-            } else if (longWord.length() == shortWord.length()) {
-                if (longWord.equals(shortWord)) {
-                    sb.append('#').append(test_case).append(" yes\n");
-                } else {
-                	sb.append('#').append(test_case).append(" no\n");
-                }
-                bw.write(sb.toString());
-                continue;
-            } // shortWord, longWord 지정
-			
-           // longWord 늘리기
-            // 최대공약수 구하기
-            int gcd = getGcd(longWord.length(), shortWord.length());
-            int lsm = longWord.length()*shortWord.length() / gcd;
+            sb1.append(a).append(b);
+            sb2.append(b).append(a);
             
-            for (int j = 0; j < lsm/longWord.length(); j++) {
-                sb.append(longWord);
-            }
-            String longLongWord = sb.toString();
-            sb.setLength(0);
-            
-            for (int j = 0; j < lsm/shortWord.length(); j++) {
-                sb.append(shortWord);
-            }
-            String shortShortWord = sb.toString();
-            sb.setLength(0);
-            
-            if (longLongWord.equals(shortShortWord)) {
+            if (sb1.toString().equals(sb2.toString())) {
                 sb.append('#').append(test_case).append(" yes\n");
             } else {
                 sb.append('#').append(test_case).append(" no\n");

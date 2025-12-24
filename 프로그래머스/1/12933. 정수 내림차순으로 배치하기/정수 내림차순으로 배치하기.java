1598d329104d68cfdc063054a84ea6ac3e986abe
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2025-12-24 15:36:20 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.58 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -2,30 +2,16 @@ import java.util.*;
 
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        LinkedList<Integer> numberList = new LinkedList<>(); // 링크드리스트 선언 -> 계속 교체할 거라서
-        //자릿수를 각각 떼어서 리스트에 넣고
-        int nLength = 0;
-        while ( n > 0 ) {
-            int nEachNumber = (int)(n % 10);
-            numberList.add(nEachNumber);
-            n /= 10;
-            nLength++;
+        
+        String[] digits = String.valueOf(n).split("");
+        
+        Arrays.sort(digits, Collections.reverseOrder());
+        
+        StringBuilder sb = new StringBuilder();
+        for (String digit : digits) {
+            sb.append(digit);
         }
         
-        //내림차순 성렬
-         Collections.sort(numberList, Collections.reverseOrder());
-      
-        for (int nL : numberList) {
-            System.out.println(nL);
-}
-        
-        //배열을 for로 곱하기 10을 하든지 해서 long으로 출력해준다
-        for(int i=0; i< nLength; i++) {
-            answer += numberList.get(i);
-            answer *= 10;
-        }
-        answer /= 10;
-        return answer;
+        return Long.parseLong(sb.toString());
     }
 }

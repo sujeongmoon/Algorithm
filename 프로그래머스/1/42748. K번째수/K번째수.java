bf39eb94f1c33c8dc27f04fcf72ff68a1e91517f
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2025-12-26 17:35:01 +0900

    [level 1] Title: K번째수, Time: 0.46 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,20 +3,17 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        ArrayList<Integer> rangeList = new ArrayList<>();
-        
-        // 리스트에 담아서. 정렬
         
         for (int i = 0; i < commands.length; i++) {
-            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
-                rangeList.add(array[j]);
-                
-            }
-            Collections.sort(rangeList); // 컬렉션 오름차순 정렬           
-            answer[i] = rangeList.get(commands[i][2]-1); // -1 잊지않기
-            rangeList.clear();
+            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
+            for (int j = 0; j <= commands[i][1] - commands[i][0]; j++) {
+                arr[j] = array[commands[i][0] + j - 1];
+            } 
+            
+            Arrays.sort(arr);
+            
+            answer[i] = arr[commands[i][2] - 1];
         }
-        
         return answer;
     }
 }

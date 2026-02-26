177dcad19db85a1a7c253196060fe44ea0c3cfc5
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2026-02-26 16:45:12 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+
+        String binaryN = Integer.toBinaryString(n);
+        for (int i = 0; i < binaryN.length(); i++) {
+            if (binaryN.charAt(i) == '1') {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}

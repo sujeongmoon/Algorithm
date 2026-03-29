e20d31002143419502e53530d65804c560140501
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2026-03-29 23:35:51 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.04 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        
+        a -= d;
+
+        for (int i = 0; i < included.length; i++) {
+            a += d;
+            if (included[i]) {
+                answer += a;
+            }
+        }
+        
+        return answer;
+    }
+}

870d95ccba2f811baf652ea8e415e2c5c92fa005
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2026-04-25 16:02:57 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.40 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -3,6 +3,37 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] strings, int n) {
         
+        
+        Arrays.sort(strings, (a, b) -> {
+            return Character.compare(a.charAt(n), b.charAt(n));
+            // Integer.compare썼던것처럼 Character로 wrapper해줘야함
+            // 그리고 이렇게되면 compareTo가아니라 compare임
+        });
+        
+        /*
+        Arrays.sort(strings, (a, b) -> {
+
+            return a.charAt(n).compareTo(b.charAt(n));
+
+        });
+        */
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         Arrays.sort(strings, (o1, o2) -> o1.charAt(n) == o2.charAt(n) ?
                    o1.compareTo(o2) : Character.compare(o1.charAt(n), o2.charAt(n)));
               

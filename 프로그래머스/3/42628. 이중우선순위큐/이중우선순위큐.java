ed3a37b4543038ce09d6b2bdebc2f3adc2e75ad6
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2026-05-04 16:51:15 +0900

    [level 3] Title: 이중우선순위큐, Time: 86.14 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -4,7 +4,8 @@ class Solution {
     public int[] solution(String[] operations) {
         int[] answer = new int[2];
         
-        TreeSet<Integer> set = new TreeSet<>();
+        //TreeSet<Integer> set = new TreeSet<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         
         for (String op : operations) {
             StringTokenizer st = new StringTokenizer(op);
@@ -13,23 +14,30 @@ class Solution {
             int num = Integer.parseInt(st.nextToken());
             
             if (s.equals("I")) {
-                set.add(num);
+                map.put(num, map.getOrDefault(num, 0) + 1);
+                
             } else if (s.equals("D")) {
-                if (!set.isEmpty()) {
+                if (!map.isEmpty()) {
+                    Map.Entry<Integer, Integer> entry = null; // 제너릭타입 명시해줘야함, 안 하면 incompatible types 에러, 초기값 명시도 해주기
                     if (num == 1) {
                         // 최댓값
-                        set.pollLast();
+                        entry = map.lastEntry();
                     } else if (num == -1) {
                         // 최솟값
-                        set.pollFirst();
+                        entry = map.firstEntry();
+                    }
+                    
+                    map.put(entry.getKey(), entry.getValue() - 1);
+                    if ((entry.getValue() - 1) == 0) { //entry가 예전 값을 가리키고 있음(lastEntry()/firstEntry() 메소드 특성)
+                        map.remove(entry.getKey());
                     }
                 }
             }
         }
         
-        if (!set.isEmpty()) {
-            answer[0] = set.last();
-            answer[1] = set.first();
+        if (!map.isEmpty()) {
+            answer[0] = map.lastEntry().getKey();
+            answer[1] = map.firstEntry().getKey();
         }
         
         return answer;

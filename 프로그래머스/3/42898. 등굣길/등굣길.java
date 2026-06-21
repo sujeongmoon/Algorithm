a47685ee72a5c6f6e7b9dc771bd35c58ef247de2
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2026-06-21 15:12:47 +0900

    [level 3] Title: 등굣길, Time: 0.42 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        
+        int[][] dp = new int[n+1][m+1];
+        
+        dp[1][1] = 1;
+        
+        // 웅덩이는 -1로 처리
+        for (int[] p : puddles) {
+            dp[p[1]][p[0]] = -1;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                
+                // 처음은 스킵
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                
+                // 웅덩이면 0으로 세팅
+                if (dp[i][j] == -1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007; // i+1j+1이라서 [0] 접근 가능
+                
+            }
+        }
+        
+        
+        answer = dp[n][m];
+        
+        return answer;
+    }
+}

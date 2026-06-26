c80781209732121a6d52bafbd00d3debe1e54cd9
sujeongmoon <163665929+sujeongmoon@users.noreply.github.com>
2026-06-26 19:17:29 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.28 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -2,64 +2,71 @@ import java.util.*;
 
 class Solution {
     public String solution(String new_id) {
-        StringBuilder sb1 = new StringBuilder();
         
+        // 1단계
         new_id = new_id.toLowerCase();
+        StringBuilder sb = new StringBuilder();
         
+        
+        // 2단계
         for (int i = 0; i < new_id.length(); i++) {
             char c = new_id.charAt(i);
-        
             if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_' || c == '.') {
-                sb1.append(c);
+                sb.append(c);
             }
-        }
-        
-        boolean isDot = false;
-        StringBuilder sb2 = new StringBuilder();
-        
-        for (int i = 0; i < sb1.length(); i++) {
-            char c = sb1.charAt(i);
             
-            if (c == '.') {
-                if (isDot) {
-                    continue;
-                }
-                isDot = true;
-            } else {
-                isDot = false;
-            }
-            sb2.append(c);
         }
         
-        if (sb2.charAt(0) == '.') {
-            sb2.deleteCharAt(0);
-        } 
+        // 4단계
+        ArrayList<Integer> doubleList = new ArrayList<>(); // 중복되는 부분의 인덱스 저장 (자동으로 내림차순)
         
-        if (sb2.length() > 0 && sb2.charAt(sb2.length() - 1) == '.') {
-            sb2.deleteCharAt(sb2.length()-1);
-        }
-        
-        if (sb2.length() == 0) {
-            sb2.append('a');
-        }
-        
-        if (sb2.length() >= 15) {
-            sb2.setLength(15);
-             if (sb2.length() > 0 && sb2.charAt(sb2.length() - 1) == '.') {
-                sb2.deleteCharAt(sb2.length()-1);
+        // 끝부터 돌면서, 만약 뒤가 지금 자신과 같고, .이라면 doubleList에 저장
+        for (int i = sb.length() - 2; i >= 0; i--) {
+            if ((sb.charAt(i) == sb.charAt(i+1)) && sb.charAt(i) == '.') {
+                doubleList.add(i);
             }
         }
         
+        // 중복 제거
+        for (int d : doubleList) {
+            sb.deleteCharAt(d);
+        }
         
-        if (sb2.length() <= 2) {
-            char c = sb2.charAt(sb2.length()-1);
-            System.out.println(c);
-            while (sb2.length() <= 2) {
-                sb2.append(c);
-            }
+                // 3단계 1 - 앞부분 제거
+        while (sb.length() != 0 && sb.charAt(0) == '.') {
+            sb.deleteCharAt(0);
+        }
+        
+        // 3단계 2 - 뒷부분 제거
+        while (sb.length() != 0 && sb.charAt(sb.length() - 1) == '.') {
+            sb.setLength(sb.length() - 1);
         }
         
         
-        return sb2.toString();
+        // 5단계
+        if (sb.length() == 0) {
+            sb.append('a');
+        }
+        
+        // 6단계
+        if (sb.length() >= 16) {
+            sb.setLength(15);
+        }
+        
+        // 6단계 2 - 뒷부분 .이면 제거
+        while (sb.length() != 0 && sb.charAt(sb.length() - 1) == '.') {
+            sb.setLength(sb.length() - 1);
+        }
+        
+        // 7단계 
+        if (sb.length() <= 2) {
+            
+            while (sb.length() != 3) {
+                sb.append(sb.charAt(sb.length()-1));
+            }
+            
+        }
+        
+        return sb.toString();
     }
 }
